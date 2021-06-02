bd26f106b65e7b220cf2d23e8a7d11995dddd69f
danielaleksandrov <crunchyhotline@gmail.com>
2021-06-02 14:19:42 -0500

    'main-page-styled'

---
+++ src/app/components/routes/pollution/pollution.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PollutionComponent } from './pollution.component';
+
+describe('PollutionComponent', () => {
+  let component: PollutionComponent;
+  let fixture: ComponentFixture<PollutionComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PollutionComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PollutionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/routes/reefs/reefs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reefs',
+  templateUrl: './reefs.component.html',
+  styleUrls: ['./reefs.component.sass']
+})
+export class ReefsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
