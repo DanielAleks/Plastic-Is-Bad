d2a531c9e9bf9221700bed3e34d78dbf11bbd551
danielaleksandrov <crunchyhotline@gmail.com>
2021-06-02 22:37:29 -0500

    'pollution-styled'

--- src/app/components/routes/pollution/pollution.component.ts
+++ src/app/components/routes/pollution/pollution.component.ts
@@ -3,13 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-pollution',
   templateUrl: './pollution.component.html',
-  styleUrls: ['./pollution.component.sass']
+  styleUrls: ['./pollution.component.sass'],
 })
 export class PollutionComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
